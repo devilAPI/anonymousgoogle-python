fb3243b1e4209826857b379a19c04714d6cbffb6
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2020-05-01 17:52:21 +0200

    Update search.py

--- search.py
+++ search.py
@@ -1,1 +1,5 @@
-www.google.de/search?q=tst
+#made by devilAPI
+import webbrowser as google
+search = input("What do you want to search today?")
+google.open("www.google.de/search?q=" + search)
+print("Thanks for using our service. Its open source so feel free to make your own code.")
