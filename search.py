cd514d77d9a6d26ac35a08dca4d88a612a235e0f
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2020-05-01 17:52:53 +0200

    Update search.py

--- search.py
+++ search.py
@@ -1,5 +1,5 @@
 #made by devilAPI
 import webbrowser as google
-search = input("What do you want to search today?")
+search = input("What do you want to search today? ")
 google.open("www.google.de/search?q=" + search)
 print("Thanks for using our service. Its open source so feel free to make your own code.")
