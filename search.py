d3f71ced88443ee804ba9a2ed70bb49f13f633f6
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2020-05-01 17:58:42 +0200

    Update search.py

--- search.py
+++ search.py
@@ -1,5 +1,5 @@
 #made by devilAPI
 import webbrowser as google
 search = input("What do you want to search today? ")
-google.open("www.google.de/search?q=" + search)
+google.open("www.google.de/search?client=anonymous-search-gx&q=" + search)
 print("Thanks for using our service. Its open source so feel free to make your own code.")
