98d58ae39375cb176f1671fdd23f7df498c0f77e
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2020-05-02 12:29:23 +0200

    Update search.py

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 #made by devilAPI
 import webbrowser as google
+print("Please note that you have to be logged out in order to be more anonymous.")
 search = input("What do you want to search today? ")
 google.open("https://www.google.de/search?client=anonymous-search-with-devils-tool&q=" + search)
 print("Thanks for using our service. Its open source so feel free to make your own code.")
